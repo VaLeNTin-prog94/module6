90807001541649e79784389cf0112f41011f9f42
VaLeNTin-prog94 <valentinforevo@gmail.com>
2024-08-15 19:57:08 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,33 +1,32 @@
 class Horse:
-    x_distance = 0
-    sound = 'Frrr'
+    def __init__(self):
+        self.x_distance = 0  #пройденный путь
+        self.sound = 'Frrr'  #звук, который издаёт лошадь
+        super().__init__()
 
+    #Метод "run" увеличивает x_distance на dx(изменение дистанции)
     def run(self, dx):
         self.x_distance += dx
 
 
-class MyMRO(type):  # наследование type = это метакласс
-    def mro(cls):
-        return (cls, Eagle, Horse,object)
-
-
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
-
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.y_distance += dy
-    def voice(self):
-        print(self.sound)
 
-class Pegasus(Horse,Eagle,metaclass=MyMRO):
+
+class Pegasus(Horse, Eagle):
+
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
 
     def get_pos(self):
         return self.x_distance, self.y_distance
+
     def voice(self):
         print(self.sound)
 
